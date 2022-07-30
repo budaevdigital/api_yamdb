56ec703c87878d2e9e7d3837662b29a2042cd3cd
Dmitry Budaev <budaev.digital@gmail.com>
2022-07-30 20:01:09 +0500

    дебаг

--- fast-install-linux.sh
+++ fast-install-linux.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-DOT_DIR_FILE_HOME=~/api_yamdb-master
-DOT_DIR_FILE_HOME_ZIP=~/api_yamdb-master.zip
-CURRENT_DIR = $(pwd)
+NAME_PROJECT=api_yamdb
+DOT_DIR_FILE_HOME=$NAME_PROJECT-master
+DOT_DIR_FILE_HOME_ZIP=$NAME_PROJECT-master.zip
+CURRENT_DIR=$(pwd)
+echo $CURRENT_DIR
 
 function install_and_start() {
-    cd $DOT_DIR_FILE_HOME
+    cd $DOT_DIR_FILE_HOME/
     python -m venv venv && source ./venv/bin/activate 
     python -m pip install --upgrade pip 
     python -m pip install -r requirements.txt 
-    cd api_yamdb/
+    cd $NAME_PROJECT/
     python manage.py makemigrations 
     python manage.py migrate 
     python manage.py runserver 
@@ -16,16 +18,14 @@ function install_and_start() {
 
 if [[ ! -d $DOT_DIR_FILE_HOME ]]
 then
-    cd $CURRENT_DIR 
-    wget https://github.com/budaevdigital/api_yamdb/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
+    wget https://github.com/budaevdigital/$NAME_PROJECT/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
     mkdir $DOT_DIR_FILE_HOME
     unzip $DOT_DIR_FILE_HOME_ZIP -d ~/
     rm $DOT_DIR_FILE_HOME_ZIP
     install_and_start
 else
     rm -r $DOT_DIR_FILE_HOME
-    cd $CURRENT_DIR
-    wget https://github.com/budaevdigital/api_yamdb/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
+    wget https://github.com/budaevdigital/$NAME_PROJECT/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
     mkdir $DOT_DIR_FILE_HOME
     unzip $DOT_DIR_FILE_HOME_ZIP -d ~/
     rm $DOT_DIR_FILE_HOME_ZIP
