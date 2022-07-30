9102d40e884124a51f1da6316c6e18ae5e04d788
Dmitry Budaev <budaev.digital@gmail.com>
2022-07-30 20:44:32 +0500

    исправил ошибку в тексте

--- fast-install-linux.sh
+++ fast-install-linux.sh
@@ -9,7 +9,7 @@ function install_and_start() {
     cd $DOT_DIR_FILE_HOME/
     python -m venv venv > /dev/null 2>&1
     source ./venv/bin/activate > /dev/null 2>&1
-    echo "Активация виртуального окружения venv и установка всеъ необходимых пакетов"
+    echo "Активация виртуального окружения venv и установка всех необходимых пакетов"
     python -m pip install --upgrade pip > /dev/null 2>&1
     python -m pip install -r requirements.txt > /dev/null 2>&1
     cd $NAME_PROJECT/
