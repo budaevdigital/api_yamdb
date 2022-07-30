a963fdedfd2bfa759bb09af09ee02a9d8139eb86
Dmitry Budaev <budaev.digital@gmail.com>
2022-07-30 19:55:01 +0500

    дебаг

--- fast-install-linux.sh
+++ fast-install-linux.sh
@@ -5,13 +5,13 @@ CURRENT_DIR = $(pwd)
 
 function install_and_start() {
     cd $DOT_DIR_FILE_HOME
-    python -m venv venv && source ./venv/bin/activate # > /dev/null 2>&1 && echo "Создано виртуальное окружение venv"
-    python -m pip install --upgrade pip # > /dev/null 2>&1 && echo "Обновляю pip в виртуальном окружении"
-    python -m pip install -r requirements.txt # > /dev/null 2>&1 && echo "Устанавливаю необходимые пакеты в виртуальном окружении"
+    python -m venv venv && source ./venv/bin/activate 
+    python -m pip install --upgrade pip 
+    python -m pip install -r requirements.txt 
     cd api_yamdb/
-    python manage.py makemigrations # > /dev/null 2>&1
-    python manage.py migrate # > /dev/null 2>&1 && echo "Произведены первоначальные миграции моделей"
-    python manage.py runserver # > /dev/null 2>&1 && echo "Поздравляем! Всё установлено и готово. Запускаю!"
+    python manage.py makemigrations 
+    python manage.py migrate 
+    python manage.py runserver 
 }
 
 if [[ ! -d $DOT_DIR_FILE_HOME ]]
